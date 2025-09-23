d9224ab2d6ac8cc53a5d2397a3b5525d1443b062
vichead303 <55242684+vichead303@users.noreply.github.com>
2025-09-23 19:49:21 +0900

    .
    .

---
+++ Goblin Restaurant/Assets/Scripts/Goblin.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Goblin : MonoBehaviour
+{
+    [SerializeField]
+    private int movespeed = 3;
+    [SerializeField]
+    public int cookingtime = 0;
+    [SerializeField]
+    private int cookingskill = 1;
+
+}
